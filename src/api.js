da8546fcbc499c4bc67b0d9a40e1b21db2472a92
Nirbhay-sys <119494283+Nirbhay-sys@users.noreply.github.com>
2024-02-27 23:47:13 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "0324f97e6bmsh58cc990cff64ebap1eb889jsn2fbdfb63cc57",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "7f3cd4d11842431291d8bf3c6f4fc101";
